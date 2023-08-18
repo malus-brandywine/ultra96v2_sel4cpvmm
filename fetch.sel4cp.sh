6b4f8e0708fe183a019063293083e84edd60a1c1
Nataliya Korovkina <malus.brandywine@gmail.com>
2023-08-18 13:04:56 -0400

    Add misc updates
    Add misc updates
    
    Signed-off-by: Nataliya Korovkina <malus.brandywine@gmail.com>

--- fetch.sel4cp.sh
+++ fetch.sel4cp.sh
@@ -9,4 +9,5 @@ git checkout 20f95dd6d5858be8d7d702ed834779e093cac275
 
 cp ../sel4cp.patches/*.patch .
 git am 0004-Add-support-of-seL4-with-SMC-capability.patch
-
+git am 0005-aarch64-SMC-cap-add-a-check-in-use-of-PD-s-smc.patch
+git am 0006-sel4.py-declare-ARMSMCCall-label.patch
--- fetch.sel4cp_vmm.sh
+++ fetch.sel4cp_vmm.sh
@@ -9,4 +9,4 @@ git checkout 6adaa4ddfc446ece0a35f9e5babb21945dee9a67
 cp ../sel4cp_vmm.patches/*.patch .
 git am 0005-Zynqmp-boards-firmware-calls-addressed.patch
 git am 0006-Add-Ultra96V2-support.patch
-
+git am 0007-Ultra96v2-add-README.md-and-defconfig.patch
